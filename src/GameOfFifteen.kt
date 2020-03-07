0b1c00618f109eb64a17cbaab18970f3713e71ff
Nurbek T <nurbekinho@gmail.com>
2020-03-07 06:14:57 +0100

    Minor change

--- src/GameOfFifteen.kt
+++ src/GameOfFifteen.kt
@@ -8,4 +8,5 @@ const val GAME_HEIGHT = 600.0
 fun main() {
     val canvas = document.getElementById("canvas") as HTMLCanvasElement
     val context = canvas.getContext("2d") as CanvasRenderingContext2D
+    Game(context)
 }
